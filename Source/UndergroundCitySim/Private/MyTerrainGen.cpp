eac44aeb67b39f106098e90053ace303129522ff
ZYR4I <115718716+ZYR4I@users.noreply.github.com>
2024-03-10 20:58:48 +0000

    Kind of works

--- Source/UndergroundCitySim/Private/MyTerrainGen.cpp
+++ Source/UndergroundCitySim/Private/MyTerrainGen.cpp
@@ -18,7 +18,7 @@ AMyTerrainGen::AMyTerrainGen()
 	{
 		UE_LOG(LogTemp, Error, TEXT("This shouldnt work"));
 	}
-
+	
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 

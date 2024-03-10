36dd12555b2f263adea324789d84a67f908ac181
ZYR4I <115718716+ZYR4I@users.noreply.github.com>
2024-03-10 17:51:14 +0000

    TerrainDone

--- Source/UndergroundCitySim/Private/MyTerrainGen.cpp
+++ Source/UndergroundCitySim/Private/MyTerrainGen.cpp
@@ -12,12 +12,13 @@ AMyTerrainGen::AMyTerrainGen()
 	groundMeshes = CreateDefaultSubobject<UHierarchicalInstancedStaticMeshComponent>
 	(TEXT("groundMeshes"));
 
-	ConstructorHelpers::FClassFinder<AMyWallCollision>wallCollisionBP(TEXT("/Game/Blueptints/WorldGen/BP_EnhancedWallCollision"));
+	static ConstructorHelpers::FClassFinder<AMyWallCollision>wallCollisionBP(TEXT("/Game/Blueptints/WorldGen/BP_EnhancedWallCollision"));
 	wallCollision = wallCollisionBP.Class;
-	if (wallCollision = NULL)
+	if (wallCollision == NULL)
 	{
 		UE_LOG(LogTemp, Error, TEXT("This shouldnt work"));
 	}
+
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
@@ -25,9 +26,10 @@ AMyTerrainGen::AMyTerrainGen()
 
 void AMyTerrainGen::PopulateGrid()
 {
+	groundMeshes->ClearInstances();
 	gridPoints.Empty();
 	worldOffset = ((gridSize * worldGridSize * 0.5) - (worldGridSize * 0.5));
-	
+	int tempIndex = 0;
 	for (int i = 0; i < gridSize; i++)
 	{
 		for (int j = 0; j < gridSize; j++)
@@ -36,7 +38,10 @@ void AMyTerrainGen::PopulateGrid()
 			gridPoints.Push(tempVector);
 			//AActor* NewActor = GetWorld()->SpawnActor(wallCollision->StaticClass(), tempVector, FRotator(0,0,0), spawnInfo);
 			AMyWallCollision* newWallCollision = GetWorld()->SpawnActor<AMyWallCollision>(wallCollision, tempVector, FRotator(0, 0, 0), spawnInfo);
-			newWallCollision->Index = i * j;
+			newWallCollision->Index = tempIndex ;
+			FTransform temp = FTransform(FRotator(0, 0, 0), tempVector, FVector(2, 2, 2));
+			groundMeshes->AddInstance(temp, true);
+			tempIndex++;
 
 			if ((i <= 2 || j <= 2 || i >= gridSize - 2 || j >= gridSize - 2))
 			{
@@ -66,13 +71,13 @@ void AMyTerrainGen::clearGrid()
 
 void AMyTerrainGen::hismPop()
 {
-	groundMeshes->ClearInstances();
+	//groundMeshes->ClearInstances();
 
-	for (FVector point : gridPoints)
-	{
-		FTransform temp = FTransform(FRotator(0, 0, 0), point, FVector(2, 2, 2));
-		groundMeshes->AddInstance(temp, true);
-	}
+	//for (FVector point : gridPoints)
+	//{
+	//	FTransform temp = FTransform(FRotator(0, 0, 0), point, FVector(2, 2, 2));
+	//	groundMeshes->AddInstance(temp, true);
+	//}
 }
 
 // Called when the game starts or when spawned
